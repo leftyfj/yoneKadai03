6679ebd91821c4cce9cd222cea7e7f7fae25e79d
hisa <gwall59@gmail.com>
2021-11-12 08:43:47 +0900

    キーワードをhtml,js側からpythonへ送り表示するところまで

--- kadai03.py
+++ kadai03.py
@@ -1,3 +1,11 @@
 import eel
+
+@eel.expose
+def from_js_call_python(val_from_js):
+  print('スタート！')
+  print(f'jsから取得したキーワード={val_from_js}')
+  
 eel.init('web')
-eel.start("index.html")
+
+eel.start("index.html",port=9999)
+
