07e7181cdfb84bcdc3308e45699797a856fb122f
hisa <gwall59@gmail.com>
2021-11-14 16:47:46 +0900

    キーワードをcvsファイルのリストから検索する機能まで

--- kadai03.py
+++ kadai03.py
@@ -1,11 +1,33 @@
 import eel
+import csv
 
-@eel.expose
-def from_js_call_python(val_from_js):
-  print('スタート！')
-  print(f'jsから取得したキーワード={val_from_js}')
+SOURCE_CSV_PATH = 'char.csv'
+@eel.expose #これでjsから呼び出せるようになる
+
+def python_function(search_char):
+  char_list = []
   
-eel.init('web')
+  with open(SOURCE_CSV_PATH, 'r', encoding="utf-8_sig") as csv_file:
+    char_list = csv_file.read().splitlines()
+  
+  print(char_list)
 
-eel.start("index.html",port=9999)
+  if search_char in char_list:
+    res = f'「{search_char}」の名前が登場人物リストにありました。'
+  else:
+    res = f'「{search_char}」の名前が登場人物リストにありませんでした。'
+  # word = f'検索したいキーワード=>{word}'
+  return res
+
+eel.init("web")
+
+web_app_options = {
+  "mode": "chrome-app",
+  "port": 9999,
+  "chromeFlags": [
+    "--window-size=800,600",
+    "--window-position = 0,0",
+  ]
+}
+eel.start("index.html", suppress_error=True, options=web_app_options)
 
