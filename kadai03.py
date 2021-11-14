45104803d14071c17f6a647f65c10172a797c480
hisa <gwall59@gmail.com>
2021-11-14 21:12:30 +0900

    ログ出力を設置

--- kadai03.py
+++ kadai03.py
@@ -1,19 +1,38 @@
 import eel
 import csv
+import datetime
+import traceback
 
+LOG_FILE_PATH = "./log/log_{datetime}.log"
+log_file_path = LOG_FILE_PATH.format(
+    datetime=datetime.datetime.now().strftime('%Y-%m-%d'))
+#log出力関数
+def make_log(txt):
+    now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    logStr = '[%s:%s] %s' % ('log', now, txt)
+    #ファイルに出力
+    with open(log_file_path, 'a', encoding='utf-8_sig') as f:
+        f.write(logStr + '\n')
+    print(logStr)
+    
 @eel.expose #これでjsから呼び出せるようになる
 def python_function(search_char, fileName):
-  char_list = []
-  with open(fileName, 'r', encoding="utf-8_sig") as csv_file:
-    char_list = csv_file.read().splitlines()
-  # print(char_list)
-  if search_char in char_list:
-    res = f'「{search_char}」の名前が登場人物リスト({fileName})にありました。'
-  else:
-    res = f'「{search_char}」の名前が登場人物リスト({fileName})にありませんでした。'
-  return res
-
+  try:
+    char_list = []
+    with open(fileName, 'r', encoding="utf-8_sig") as csv_file:
+      char_list = csv_file.read().splitlines()
+    # print(char_list)
+    if search_char in char_list:
+      res = f'「{search_char}」はいます'
+    else:
+      res = f'「{search_char}」はいません。' + '\n' + f'「{search_char}」を追加します。'
+    make_log('検索終了')
+    return res
+  except:
+    make_log('検索失敗')
+    
 eel.init("web")
+
 web_app_options = {
   "mode": "chrome-app",
   "port": 9999,
@@ -25,4 +44,4 @@ web_app_options = {
 # eel.start("index.html", suppress_error=True, options=web_app_options)
 
 eel.start("index.html")
-
+make_log('idex.html起動')
