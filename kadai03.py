1b458a07a8619547ffbc79aaf5691a290d8ca7d1
hisa <gwall59@gmail.com>
2021-11-15 20:52:30 +0900

    git ignore commitしないファイルを追加他

--- kadai03.py
+++ kadai03.py
@@ -2,6 +2,7 @@ import eel
 import csv
 import datetime
 import traceback
+import socket 
 
 LOG_FILE_PATH = "./log/log_{datetime}.log"
 log_file_path = LOG_FILE_PATH.format(
@@ -36,16 +37,20 @@ def python_function(search_char, fileName):
     make_log('検索失敗')
     
 eel.init("web")
-
+# 未使用ポート取得
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 0))
+port = s.getsockname()[1]
+s.close()
 web_app_options = {
-  "mode": "chrome-app",
-  "port": 9999,
+  "mode": "chrome",
+  "port": port,
   "chromeFlags": [
     "--window-size=800,600",
     "--window-position = 0,0",
   ]
 }
-# eel.start("index.html", suppress_error=True, options=web_app_options)
+eel.start("index.html", suppress_error=True, options=web_app_options)
 
-eel.start("index.html")
+# eel.start("index.html")
 make_log('idex.html起動')
